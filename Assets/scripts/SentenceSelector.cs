bba1d001fdd3a5c30995877cc49841a19742ff7a
Pete Y <pyork31@googlemail.com>
2018-01-27 13:50:04 +0000

    add word length files

--- Assets/scripts/SentenceSelector.cs
+++ Assets/scripts/SentenceSelector.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using System;
 using System.Xml;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SentenceSelector : MonoBehaviour {
-	XmlDocument dinosaurComicsXML;
+	private static String textDirectory = "D:\\Unity\\Projectz\\Chinese Dinosaur Whispers Comic\\Assets\\dinosaurText\\";
+	private static int maxWordCount = 100;
+	private XmlDocument dinosaurComicsXML;
 
 	void Start () {
 		dinosaurComicsXML = new XmlDocument ();
-		dinosaurComicsXML.Load ("D:\\Unity\\Projectz\\Chinese Dinosaur Whispers Comic\\Assets\\everywordindinosaurcomicsOHGOD.xml");
+		dinosaurComicsXML.Load (textDirectory + "everywordindinosaurcomicsOHGOD.xml");
 
 		String firstLine = GetRandomLine ();
 		SetString (firstLine);
+
+		ParseAndCreateFiles ();
 	}
 
 	private String GetFirstLine () {
@@ -42,5 +47,31 @@ public class SentenceSelector : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			SetString (GetRandomLine ());
 		}
+	}
+
+
+
+	private void ParseAndCreateFiles() {
+		List<StreamWriter> streamWriters = new List<StreamWriter> ();
+		for (int i = 0; i < maxWordCount; i++) {
+			streamWriters.Add(new StreamWriter(textDirectory + i + ".txt"));
+		}
+		XmlNodeList lines = dinosaurComicsXML.GetElementsByTagName ("line");
+		foreach (XmlNode line in lines) {
+			String lineText = line.InnerText;
+			int wordCount = GetWordCount (lineText);
+			if (wordCount < maxWordCount) {
+				WriteLine (lineText, streamWriters[wordCount]);
+			}
+		}	
+	}
+
+	private int GetWordCount (String text){
+		String[] words = text.Split (' ');
+		return words.Length;
+	}
+
+	private void WriteLine (String text, StreamWriter file) {
+		file.WriteLine (text);
 	}
 }
